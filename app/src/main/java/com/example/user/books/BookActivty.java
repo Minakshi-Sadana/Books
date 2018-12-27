a07057c22c1fd4ece15230052960ace044433ab1
User <sadanaminakshi4@gmail.com>
2018-12-27 07:45:57 +0530

    Images permission

--- app/src/main/java/com/example/user/books/BookActivty.java
+++ app/src/main/java/com/example/user/books/BookActivty.java
@@ -1,9 +1,14 @@
 package com.example.user.books;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,25 +23,50 @@ import java.util.ArrayList;
 public class BookActivty extends AppCompatActivity {
     ImageView imageView;
     ArrayList<String> imagePaths=new ArrayList<>();
+    private static final int MY_PERMSSIONS=1;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_activty);
        // imageView=findViewById(R.id.bookImage);
-        ViewPager viewPager=findViewById(R.id.pager);
-    /**    getBitmapPublicFile();
-        ImageAdapter adapter=new ImageAdapter(this,imagePaths);
-        viewPager.setAdapter(adapter);**/
-        Intent intent=new Intent(BookActivty.this,MP3.class);
-        startActivity(intent);
+        viewPager=findViewById(R.id.pager);
+        if(ContextCompat.checkSelfPermission(BookActivty.this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            getBitmapPublicFile();
+            ImageAdapter adapter=new ImageAdapter(this,imagePaths);
+            viewPager.setAdapter(adapter);
+        }else{
+            //Toast.makeText(getApplicationContext(),"Permission not granted",Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(BookActivty.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},MY_PERMSSIONS);
+        }
+
+
+    //    Intent intent=new Intent(BookActivty.this,MP3.class);
+    //    startActivity(intent);
     }
 
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        switch (requestCode){
+            case MY_PERMSSIONS:{
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    getBitmapPublicFile();
+                    ImageAdapter adapter=new ImageAdapter(this,imagePaths);
+                    viewPager.setAdapter(adapter);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Permission denied",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+
+    }
     private void getBitmapPublicFile() {
         String MEDIA_MOUNTED="mounted";
         String diskState=Environment.getExternalStorageState();
         if(diskState.equals(MEDIA_MOUNTED)){
-            File pictureFolder=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            File pictureFolder=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
             File[] files=pictureFolder.listFiles();
             imagePaths=new ArrayList<>();
             for(int i=0;i<files.length;i++) {
